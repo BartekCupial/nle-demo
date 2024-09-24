1d36ee3da72415fb09eeea4d148a96fb1b9be270
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-09-24 14:57:21 +0200

    play all babaisai envs

---
+++ nle_demo/envs/babaisai/record_all_babaisai.py
@@ -0,0 +1,30 @@
+import sys
+
+from nle_utils.cfg.arguments import parse_args, parse_full_cfg
+from nle_utils.play import play
+from nle_utils.utils.context import global_env_registry
+
+from nle_demo.cfg.cfg import add_extra_params_demo
+from nle_demo.envs.babaisai.babaisai_params import add_extra_params_babaisai_env
+from nle_demo.envs.babaisai.record_babaisai import get_action, register_babaisai_components
+
+
+def parse_babaisai_args(argv=None):
+    argv = sys.argv[1:] + argv
+    parser, partial_cfg = parse_args(argv=argv)
+    add_extra_params_babaisai_env(parser)
+    add_extra_params_demo(parser)
+    final_cfg = parse_full_cfg(parser, argv)
+    return final_cfg
+
+
+def main():
+    register_babaisai_components()
+    env_registry = global_env_registry()
+    for env_name in env_registry.keys():
+        cfg = parse_babaisai_args(argv=[f"--env={env_name}"])
+        play(cfg, get_action=get_action)
+
+
+if __name__ == "__main__":
+    main()
